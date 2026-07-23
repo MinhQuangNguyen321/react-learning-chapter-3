e1a19352d17910470c579ce99ab36d2accbf7d59
Quang Nguyen Minh <nmquang132007@gmail.com>
2026-07-23 18:41:34 +0700

    lesson 23

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,18 @@
 import React from "react";
 import UserInfor from "./UserInfor";
+import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
   render() {
+    const myInfor = ["ab", "b", "c"];
     return (
       <div>
         <UserInfor />
+        <br />
+        <br />
+        <DisplayInfor name="QNM" age="19" />
+        <hr></hr>
+        <DisplayInfor name={"Bin"} age={26} myInfor={myInfor} />
       </div>
     );
   }
